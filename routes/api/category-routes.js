a879f5e434fd78e334c374d57e8228602837af51
Elsa Balkaran <elsabalk@hotmail.com>
2021-09-27 16:58:25 -0500

    Refactor route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,20 +4,21 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 router.get('/', (req, res) => {
   // find all categories
+    // be sure to include its associated Products
     Category.findAll({
-      attributes: ['id', 'category_name'],
-      include: [
-          {
-              model: Product,
-              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-          }
-      ]
-  })
-      .then(allCategoryData => res.json(allCategoryData))
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+        attributes: ['id', 'category_name'],
+        include: [
+            {
+                model: Product,
+                attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+            }
+        ]
+    })
+        .then(allCategoryData => res.json(allCategoryData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
