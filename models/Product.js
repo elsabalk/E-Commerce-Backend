209278561a1917ac0f872ee26f6274d60256d638
Elsa Balkaran <elsabalk@hotmail.com>
2021-09-27 13:37:58 -0500

    Fixed product model tag issue, updated index.js with foreign key relationships, updated server.js with connection path

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,9 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+class Product extends Model {
+
+}
 
 // set up fields and rules for Product model
 Product.init(
@@ -38,6 +40,8 @@ Product.init(
         isNumeric: true
       }
     },
+
+  },
   {
     sequelize,
     timestamps: false,
@@ -45,6 +49,8 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+
 );
+
 
 module.exports = Product;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+
+
 // import sequelize connection
+const seq = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
